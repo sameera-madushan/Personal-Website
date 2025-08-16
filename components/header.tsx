76458dc1c0c49cc8056ac38ad64ab26369ccc83f
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-08-16 18:13:46 +0530

    Created site header and footer components

---
+++ components/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import LogoToggle from '@/components/logo-toggle'
+import ThemeToggle from '@/components/theme-toggle'
+
+export default function Header() {
+  
+  return (
+    <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm'>
+      <nav className='container flex max-w-3xl items-center justify-between'>
+        <LogoToggle />
+
+        <ul className='flex items-center gap-6 text-sm font-light text-muted-foreground sm:gap-10'>
+          <li className='transition-colors hover:text-foreground'>
+            <Link href='/posts'>Posts</Link>
+          </li>
+          <li className='transition-colors hover:text-foreground'>
+            <Link href='/projects'>Projects</Link>
+          </li>
+          <li className='transition-colors hover:text-foreground'>
+            <Link href='/contact'>Contact</Link>
+          </li>
+        </ul>
+
+        <div>
+          <ThemeToggle />
+        </div>
+      </nav>
+    </header>
+  )
+}
