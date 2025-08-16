071addbc7e753645ac2ae698cc78922c09422684
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-08-16 18:19:43 +0530

    Added Providers wrapper with next-themes for dark/light mode

---
+++ components/providers.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { ThemeProvider, useTheme } from "next-themes"
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+        </ThemeProvider>
+    )
+}
