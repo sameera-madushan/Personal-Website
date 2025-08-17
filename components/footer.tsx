85b9a1d8226c440a7b308351cacaaad070fc395c
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-08-17 22:22:45 +0530

    Update footer container to 'mx-auto' and remove X icon

--- components/footer.tsx
+++ components/footer.tsx
@@ -40,15 +40,6 @@ const navigation = [
     )
   },
   {
-    name: 'X',
-    href: '#',
-    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
-      <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
-        <path d='M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z' />
-      </svg>
-    )
-  },
-  {
     name: 'GitHub',
     href: 'https://github.com/sameera-madushan',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
@@ -66,7 +57,7 @@ const navigation = [
 export default function Footer() {
   return (
     <footer className='py-8'>
-      <div className='container max-w-3xl'>
+      <div className='mx-auto max-w-3xl'>
         <div className='md:flex md:items-center md:justify-between'>
           <div className='flex justify-center space-x-6 md:order-2'>
             {navigation.map(item => (
