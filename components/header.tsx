f6c0a7c7d34975684568656256b971930e2dec09
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-08-17 22:30:38 +0530

    Centered the nav content with mx-auto

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   
   return (
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm'>
-      <nav className='container flex max-w-3xl items-center justify-between'>
+      <nav className='mx-auto flex max-w-3xl items-center justify-between'>
         <LogoToggle />
 
         <ul className='flex items-center gap-6 text-sm font-light text-muted-foreground sm:gap-10'>
