f7d477f03934f9dcba66ed13f3d527a6a95a5fc0
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-08-17 22:38:49 +0530

    Created ContactForm component with validation, submission, and toast notifications

---
+++ components/contact-form.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import { z } from 'zod'
+import Link from 'next/link'
+import { toast } from 'sonner'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ContactFormSchema } from '@/schemas/contact-form-schema'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { sendEmail } from '@/lib/actions'
+import { Toaster } from "@/components/ui/sonner"
+
+type Inputs = z.infer<typeof ContactFormSchema>
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting }
+  } = useForm<Inputs>({
+    resolver: zodResolver(ContactFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      message: ''
+    }
+  })
+
+  const processForm: SubmitHandler<Inputs> = async data => {
+    const result = await sendEmail(data)
+
+    if (result?.error) {
+      toast.error('An error occurred! Please try again.')
+      return
+    }
+
+    toast.success('Message sent successfully!')
+    reset()
+  }
+
+  return (
+    <section className='relative isolate'>
+      <Toaster position="top-right"/>
+      {/* Form */}
+      <div className='relative'>
+        <form
+          onSubmit={handleSubmit(processForm)}
+          className='mt-16 lg:flex-auto'
+          noValidate
+        >
+          <div className='grid grid-cols-1 gap-6 sm:grid-cols-2'>
+            {/* Name */}
+            <div>
+              <Input
+                id='name'
+                type='text'
+                placeholder='Your Name'
+                autoComplete='given-name'
+                {...register('name')}
+              />
+
+              {errors.name?.message && (
+                <p className='ml-1 mt-2 text-sm text-rose-400'>
+                  {errors.name.message}
+                </p>
+              )}
+            </div>
+
+            {/* Email */}
+            <div>
+              <Input
+                type='email'
+                id='email'
+                autoComplete='email'
+                placeholder='Your Email'
+                {...register('email')}
+              />
+
+              {errors.email?.message && (
+                <p className='ml-1 mt-2 text-sm text-rose-400'>
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            {/* Message */}
+            <div className='sm:col-span-2'>
+              <Textarea
+                rows={4}
+                placeholder='Message'
+                {...register('message')}
+              />
+
+              {errors.message?.message && (
+                <p className='ml-1 mt-2 text-sm text-rose-400'>
+                  {errors.message.message}
+                </p>
+              )}
+            </div>
+          </div>
+          <div className='mt-6'>
+            <Button
+              type='submit'
+              disabled={isSubmitting}
+              className='w-full disabled:opacity-50'
+            >
+              {isSubmitting ? 'Submitting...' : 'Submit'}
+            </Button>
+          </div>
+          <p className='mt-4 text-xs text-muted-foreground'>
+            By submitting this form, I agree to the{' '}
+            <Link href='/privacy' className='font-bold'>
+              privacy&nbsp;policy.
+            </Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  )
+}
