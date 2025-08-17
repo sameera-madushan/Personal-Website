08045e257cccfb6d0c91fcf1a408fb2210d45afe
Sameera Madushan <sameera.xyz.me@gmail.com>
2025-08-17 22:24:09 +0530

    Added RecentPosts, RecentProjects, and center content with mx-auto

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Intro from "@/components/intro";
+import RecentPosts from '@/components/recent-posts'
+import RecentProjects from '@/components/recent-projects'
 
 export default function Home(){
   return (
     <section className="py-24">
-      <div className="container max-w:3xl">
+      <div className="mx-auto max-w-3xl px-4">
         <Intro />
+        <RecentPosts />
+        <RecentProjects />
       </div>
     </section>
   )
